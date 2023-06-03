3b81871adf529fa89445ec0d7d157c83cca1658d
Xac47 <hasbulat69@gmail.com>
2023-06-03 20:28:56 +0300

    start project

---
+++ base/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+import uuid
+
+
+class BaseModel(models.Model):
+    uid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('-created_at')
+        indexes = [
+            models.Index(fields=['-created_at'])
+        ]
