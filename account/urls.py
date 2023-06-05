c24c645f8f69da17f70b1c919218d9b1dd244d37
Xac47 <hasbulat69@gmail.com>
2023-06-05 20:39:16 +0300

    add url passworda_reset and password_reset_confirm

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView
 
 from account import views
+from account.forms import PasswordResetForm, SetPasswordForm
 
 urlpatterns = [
     path('login/', views.MyLoginView.as_view(), name='login'),
+
+    path('password_reset/', PasswordResetView.as_view(form_class=PasswordResetForm), name='password_reset'),
+    path("reset/<uidb64>/<token>/", PasswordResetConfirmView.as_view(form_class=SetPasswordForm), name="password_reset_confirm"),
+
     path('', include('django.contrib.auth.urls')),
 
     path('confirm_email/', TemplateView.as_view(template_name='registration/confirm_email.html'),
