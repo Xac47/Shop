413f9545b6403d0eaaa560292d96206dab51e28e
Xac47 <hasbulat69@gmail.com>
2023-06-06 14:34:15 +0300

    update file PEP8

--- account/utils.py
+++ account/utils.py
@@ -5,6 +5,8 @@ from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator as token_generator
 
+from random import choice
+from string import ascii_letters, digits
 
 def send_email_for_verify(request, user):
     current_site = get_current_site(request)
@@ -26,9 +28,6 @@ def send_email_for_verify(request, user):
     email.send()
 
 def security_code(length=4):
-    from random import choice
-    from string import ascii_letters, digits
-
     alf = ascii_letters + digits
     security_code = ''.join([choice(alf) for _ in range(length)])
 
