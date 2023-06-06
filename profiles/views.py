71a4572d200ccc83fd693e07b8d2b4a6f46d1efa
Xac47 <hasbulat69@gmail.com>
2023-06-06 16:09:05 +0300

    add function my_profile in file profiles/views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def my_profile(request):
+    profile = request.user
+
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, 'profile/profile.html', context)
