5de9e6326359f441cda27e6cb74de82fe2391643
Xac47 <hasbulat69@gmail.com>
2023-06-08 16:14:41 +0300

    change LOGIN_REDIRECT_URL in file config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -137,7 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.User'
 
-LOGIN_REDIRECT_URL = 'profile'
+LOGIN_REDIRECT_URL = 'profiles:profile'
 LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL = 'login'
 
