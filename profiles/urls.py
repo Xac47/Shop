31c658142548d66fd7d3102203a8e82195f86a6e
Xac47 <hasbulat69@gmail.com>
2023-06-08 16:15:47 +0300

    add urls my_address, my_orders, update_profile in file profiles/urls.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,4 +6,7 @@ app_name = 'profiles'
 
 urlpatterns = [
     path('', views.my_profile, name='profile'),
+    path('my_address/', views.my_address, name='my_address'),
+    path('my_orders/', views.my_orders, name='my_orders'),
+    path('update/', views.update_profile, name='update_profile'),
 ]
