6de6a6b8ce40df92f11adbcb06206536dc5447e4
Xac47 <hasbulat69@gmail.com>
2023-06-08 16:16:45 +0300

    add view  my_orders, update_profile in file in file profiles/views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from profiles.forms import UpdateDataProfileForm
+
 
 @login_required
 def my_profile(request):
@@ -10,4 +12,41 @@ def my_profile(request):
         'profile': profile,
     }
 
-    return render(request, 'profile/profile.html', context)
+    return render(request, 'profiles/profile.html', context)
+
+
+@login_required
+def my_address(request):
+    profile = request.user
+
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, 'profiles/my_address.html', context)
+
+
+@login_required
+def my_orders(request):
+    profile = request.user
+
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, 'profiles/my_orders.html', context)
+
+@login_required
+def update_profile(request):
+    profile = request.user
+    form = UpdateDataProfileForm(instance=profile)
+    if request.POST:
+        form = UpdateDataProfileForm(request.POST, instance=profile)
+        form.save()
+
+    context = {
+        'profile': profile,
+        'form': form
+    }
+
+    return render(request, 'profiles/update_profile.html', context)
