a559b2e626eb055089f0542e04e0713042515545
Xac47 <hasbulat69@gmail.com>
2023-06-13 11:44:42 +0300

    add in INSTALLED_APPS (phonenumber_field) in file config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-
+    'phonenumber_field',
 
     'account.apps.AccountConfig',
     'product.apps.ProductConfig',
