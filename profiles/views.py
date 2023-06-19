5a8cc3993d4898282e9115d227696f4ead99e379
Xac47 <hasbulat69@gmail.com>
2023-06-20 02:19:50 +0300

    add class UpdateProfileView

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,15 @@
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import UpdateView
 
 from profiles.forms import UpdateDataProfileForm
+
+User = get_user_model()
 
 
 @login_required
@@ -37,20 +44,13 @@ def my_orders(request):
 
     return render(request, 'profiles/my_orders.html', context)
 
-@login_required
-def update_profile(request):
-    profile = request.user
-    form = UpdateDataProfileForm(instance=profile)
 
-    if request.method == 'POST':
-        form = UpdateDataProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            messages.success(request, 'Данные успешно обновлены')
-            form.save()
+@method_decorator(login_required, name='dispatch')
+class UpdateProfileView(SuccessMessageMixin, UpdateView):
+    form_class = UpdateDataProfileForm
+    template_name = 'profiles/update_profile.html'
+    success_message = 'Данные успешно обновлены'
+    success_url = reverse_lazy('profiles:update_profile')
 
-    context = {
-        'profile': profile,
-        'form': form
-    }
-
-    return render(request, 'profiles/update_profile.html', context)
+    def get_object(self, queryset=None):
+        return self.request.user
