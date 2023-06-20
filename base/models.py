dd8379d9df70982d94900f37f6aaa45b568e6810
Xac47 <hasbulat69@gmail.com>
2023-06-20 20:52:48 +0300

    add class ImageBaseModel

--- base/models.py
+++ base/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.utils.safestring import mark_safe
+
+
+class ImageBaseModel(models.Model):
+    image = None
+
+    def get_image(self, width=50, height=50):
+        return mark_safe(f'<img src={self.image.url} width="{width}" height="{height}">')
+
+    class Meta:
+        abstract = True
 
 
 class BaseModel(models.Model):
