173fcc12fed124421640c0a44e67dad5776a0bdc
Xac47 <hasbulat69@gmail.com>
2023-06-21 03:22:50 +0300

    update model BaseModel: add atribute slug, remote indexes and ordering

--- base/models.py
+++ base/models.py
@@ -15,14 +15,10 @@ class ImageBaseModel(models.Model):
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    slug = models.SlugField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=255)
 
     class Meta:
         abstract = True
-        ordering = ('-created_at',)
-        indexes = [
-            models.Index(fields=['-created_at'])
-        ]
 
 
 class Social_links(models.Model):
