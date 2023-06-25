3b2bf946d185410d07afb64819e072e5f48a6f3c
Xac47 <hasbulat69@gmail.com>
2023-06-26 01:46:20 +0300

    update method get_image in class ImageBaseModel

--- base/models.py
+++ base/models.py
@@ -6,7 +6,9 @@ class ImageBaseModel(models.Model):
     image = None
 
     def get_image(self, width=50, height=50):
-        return mark_safe(f'<img src={self.image.url} width="{width}" height="{height}">')
+        if self.image:
+            return mark_safe(f'<img src={self.image.url} width="{width}" height="{height}">')
+        return 'NO PHOTO'
 
     class Meta:
         abstract = True
