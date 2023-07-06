240df7a7bdd64c96e00a6a0e1bfc82fd456f0632
Xac47 <hasbulat69@gmail.com>
2023-07-06 17:38:21 +0300

    add class for class Tag, Category

--- product/admin.py
+++ product/admin.py
@@ -4,8 +4,25 @@ from django.utils.text import slugify
 
 from product.models import Product, ProductImages, Category, Tag
 
-admin.site.register(Category)
-admin.site.register(Tag)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    list_display_links = ('id', 'name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'get_image')
+    list_display_links = ('id', 'name', 'slug', 'get_image')
+    readonly_fields = ('get_image',)
+    prepopulated_fields = {'slug': ('name',)}
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="50">')
+
+    get_image.short_description = "Изображение"
 
 
 class ProductImagesInline(admin.TabularInline):
