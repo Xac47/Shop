2211195bc5f9895dd52212d4be747f5d5d9d5ae1
Xac47 <hasbulat69@gmail.com>
2023-07-06 12:43:08 +0300

    add classes in file product/admin.py

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.utils.text import slugify
 
-# Register your models here.
+from product.models import Product, ProductImages, Category, Tag
+
+admin.site.register(Category)
+admin.site.register(Tag)
+
+
+class ProductImagesInline(admin.TabularInline):
+    model = ProductImages
+    extra = 0
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="100" height="100">')
+
+    get_image.short_description = "Изображение"
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'slug', 'status', 'category', 'price', 'author', 'get_image')
+    list_display_links = ('id', 'title', 'slug', 'status', 'category', 'price', 'author', 'get_image')
+    readonly_fields = ('get_image',)
+    inlines = (ProductImagesInline,)
+    prepopulated_fields = {'slug': ('title',)}
+
+    save_as = True
+    save_on_top = True
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="50">')
+
+    get_image.short_description = "Изображение"
