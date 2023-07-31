5c5bbf2711a7621740597c60ccfc819966e2c87a
Xac47 <hasbulat69@gmail.com>
2023-07-31 13:15:06 +0300

    add url addres: search, products, discount_product, category, product_detail

--- product/urls.py
+++ product/urls.py
@@ -6,4 +6,10 @@ app_name = 'product'
 
 urlpatterns = [
     path('', views.HomeListView.as_view(), name='home'),
+    path('search/', views.SearchView.as_view(), name='search'),
+    path('products/', views.ProductListView.as_view(), name='products'),
+    path('discounted_products/', views.DiscountProductListView.as_view(), name='discount_product'),
+
+    path('category/<slug:category_slug>/', views.CategoryListView.as_view(), name='category'),
+    path('product/<slug:category_slug>/<int:pk>/<slug:product_slug>/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
