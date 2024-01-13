6479bd6046b22e7e29f2228d385713b9f83898e9
Xac47 <hasbulat69@gmail.com>
2024-01-13 15:36:31 +0300

    create model subscribeEmail

--- subscribe_email/models.py
+++ subscribe_email/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from base.models import BaseModel
+
+
+class SubscribeEmail(BaseModel):
+    email = models.EmailField(unique=True, max_length=255)
+
+    class Meta:
+        verbose_name = 'Подписка на рассылку'
+        verbose_name_plural = 'Подписки на рассылку'
+
+
+    def __str__(self):
+        return self.email
