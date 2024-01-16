9805258ef89d109360371a2167658fe158a19d63
Xac47 <hasbulat69@gmail.com>
2024-01-16 14:51:06 +0300

    add class AddSubscribeEmailView

--- subscribe_email/views.py
+++ subscribe_email/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView
 
-# Create your views here.
+from subscribe_email.forms import SubscribeEmailForm
+from subscribe_email.models import SubscribeEmail
+
+
+class AddSubscribeEmailView(CreateView):
+    form_class = SubscribeEmailForm
+    template_name = 'product/home_list.html'
+    success_url = reverse_lazy('product:home')
