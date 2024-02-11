346019a80d7d3371706fd6ac5b4b73d93e46059d
Xac47 <hasbulat69@gmail.com>
2024-02-11 15:50:30 +0300

    add app orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email',
+                    'phone', 'address', 'postal_code', 'city', 'paid',
+                    'created', 'updated']
+    list_display_links = ['id', 'first_name', 'last_name', 'email', 'phone', 'address', 'postal_code', 'city']
+    list_filter = ['paid', 'created', 'updated']
+    inlines = [OrderItemInline]
+
+    actions = ['mark_as_paid']
+
+    def mark_as_paid(self, request, queryset):
+        queryset.update(paid=True)
