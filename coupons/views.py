8316cb5d4d4458e821047705d205431646f2953e
Xac47 <hasbulat69@gmail.com>
2024-02-11 15:56:17 +0300

    update method coupon_apply

--- coupons/views.py
+++ coupons/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.views.decorators.http import require_POST
+from django.utils import timezone
+from .models import Coupon
+from .forms import CouponApplyForm
 
-# Create your views here.
+from django.core.exceptions import ObjectDoesNotExist
+
+
+@require_POST
+def coupon_apply(request):
+    now = timezone.now()
+    form = CouponApplyForm(request.POST)
+    if form.is_valid():
+        code = form.cleaned_data['code']
+        try:
+            coupon = Coupon.active.get(code__iexact=code,  # поиск совпадения
+                                       valid_from__lte=now,  # проверка срока меньше или равными пояс | <=
+                                       valid_to__gte=now)  # проверка срока больше или равными поле | >=
+            request.session['coupon_id'] = coupon.id
+            messages.success(request, f'Купон применен {coupon.discount}%')
+        except ObjectDoesNotExist:
+            messages.error(request, 'Купон не существует')
+            request.session['coupon_id'] = None
+    return redirect('cart:cart_detail')
