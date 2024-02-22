80d0e01fc0471cec86b745a3d0c8a937d05f302b
Xac47 <hasbulat69@gmail.com>
2024-02-22 18:13:02 +0300

    update form

--- orders/forms.py
+++ orders/forms.py
@@ -6,6 +6,24 @@ class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'phone', 'postal_code', 'city']
+        labels = {
+            'first_name': '',
+            'last_name': '',
+            'email': '',
+            'address': '',
+            'phone': '',
+            'postal_code': '',
+            'city': '',
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': 'Фамилия*'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Имя*'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email*'}),
+            'address': forms.TextInput(attrs={'placeholder': 'Адрес*'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Телефон*'}),
+            'postal_code': forms.TextInput(attrs={'placeholder': 'Индекс*'}),
+            'city': forms.TextInput(attrs={'placeholder': 'Город*'}),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
