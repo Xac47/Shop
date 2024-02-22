5e6b70730e66de07978f66055f760638252a7904
Xac47 <hasbulat69@gmail.com>
2024-02-22 18:10:14 +0300

    update danger method iter

--- cart/cart.py
+++ cart/cart.py
@@ -54,6 +54,8 @@ class Cart:
         for item in self.cart.values():
             item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['quantity']
+            item['discount_price'] = (self.coupon.discount / Decimal('100')) * (
+                    item['price'] * item['quantity']) if self.coupon else item['price'] * item['quantity']
             yield item
 
     def __len__(self):
