bf68acd7ce27513b7a3aa0d64e3114810b93d6a9
Xac47 <hasbulat69@gmail.com>
2024-02-26 13:29:56 +0300

    add address admin_order_detail

--- orders/urls.py
+++ orders/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'orders'
 
 urlpatterns = [
+    path('admin/order/<order_id>/', views.admin_order_detail, name='admin_order_detail'),
     path('', views.OrderListView.as_view(), name='order_list'),
     path('<int:order_id>/', views.order_detail, name='order_detail'),
     path('create/', views.order_create, name='order_create'),
