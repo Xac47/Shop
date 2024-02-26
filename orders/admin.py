273c6f05a066d36fce4a7987b89409a8413517e5
Xac47 <hasbulat69@gmail.com>
2024-02-26 13:29:10 +0300

    add fun export_to_csv

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,34 @@
 from django.contrib import admin
+from django.http import HttpResponse
+
 from .models import Order, OrderItem
+
+import csv
+import datetime
+
+
+def export_to_csv(modeladmin, request, queryset):
+    ''' Скачивание файлов в формате csv '''
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Напишите первую строку с информацией заголовка
+    writer.writerow([field.verbose_name for field in fields])
+    # Запись строк данных
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'
 
 
 class OrderItemInline(admin.TabularInline):
@@ -16,7 +45,10 @@ class OrderAdmin(admin.ModelAdmin):
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
 
-    actions = ['mark_as_paid']
+    actions = ['export_to_csv', 'mark_as_paid']
+
+    def export_to_csv(self, request, queryset):
+        return export_to_csv(self, request, queryset)
 
     def mark_as_paid(self, request, queryset):
         queryset.update(paid=True)
