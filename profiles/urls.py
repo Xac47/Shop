5b28ab7b9cbcf5fbffa96e416b66ab43c281bf97
Xac47 <hasbulat69@gmail.com>
2024-03-02 16:49:28 +0300

    delete url address my_address

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,9 +6,7 @@ app_name = 'profiles'
 
 urlpatterns = [
     path('', views.my_profile, name='profile'),
-    path('my_address/', views.my_address, name='my_address'),
     path('update/', views.UpdateProfileView.as_view(), name='update_profile'),
-
     path('favorites/<int:product_id>/<slug:product_slug>/', views.favorites_add_or_remove,
          name='favorites_add_or_remove'),
 ]
