a95d294ae038b4ab1457e429e69609de1cda668b
Xac47 <hasbulat69@gmail.com>
2024-03-02 16:50:20 +0300

    update funct order_create

--- orders/views.py
+++ orders/views.py
@@ -39,11 +39,14 @@ def order_create(request):
         if form.is_valid():
             order = form.save(commit=False)
             order.user = request.user
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.coupon.discount
             order.save()
             for item in cart:
                 OrderItem.objects.create(order=order,
                                          product=item['product'],
-                                         price=item['discount_price'],
+                                         price=item['total_price'],
                                          quantity=item['quantity'])
             # очистка корзины
             cart.clear()
